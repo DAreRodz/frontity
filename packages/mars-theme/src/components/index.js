a27b3bc7a2041738656c0f5c7594c6dc4e7d6e29
David Arenas <darensr@gmail.com>
2020-02-11 11:33:36 +0100

    MarsTheme: Add swipe proof of concept

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Global, css, connect, styled, Head } from "frontity";
 import Header from "./header";
 import List from "./list";
-import Post from "./post";
+import Viewpager from "./viewpager";
 import Loading from "./loading";
 import Title from "./title";
 import PageError from "./page-error";
@@ -14,6 +14,9 @@ import PageError from "./page-error";
 const Theme = ({ state }) => {
   // Get information about the current URL.
   const data = state.source.get(state.router.link);
+
+  // Get list of links to be used by Viewpager.
+  const links = state.source.get(state.source.homepage).items.map(i => i.link);
 
   return (
     <>
@@ -36,9 +39,9 @@ const Theme = ({ state }) => {
       {/* Add the main section. It renders a different component depending
       on the type of URL we are in. */}
       <Main>
-        {(data.isFetching && <Loading />) ||
+        {(data.isPostType && <Viewpager links={links} />) ||
+          (data.isFetching && <Loading />) ||
           (data.isArchive && <List />) ||
-          (data.isPostType && <Post />) ||
           (data.isError && <PageError />)}
       </Main>
     </>
